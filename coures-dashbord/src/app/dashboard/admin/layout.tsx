b8b1675872e2dfb691e57793a42721b82082c7fb
Awad Reda <awadreda496@gmail.com>
2025-07-06 07:48:37 +0300

    feat: initialize course dashboard with Next.js and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Created postcss.config.mjs for Tailwind CSS integration
    - Added SVG assets for icons and branding
    - Implemented admin layout with navigation links
    - Developed admin dashboard page structure
    - Created teacher dashboard page (currently empty)
    - Added favicon for the application
    - Defined global styles with Tailwind CSS
    - Set up root layout for the application
    - Created home page with a welcome message
    - Configured TypeScript settings for the project

---
+++ coures-dashbord/src/app/dashboard/admin/layout.tsx
@@ -0,0 +1,51 @@
+import Link from 'next/link'
+import React from 'react'
+
+type Props = {}
+
+export default function layout ({ children }: { children: React.ReactNode }) {
+  return (
+    <div className='min-h-screen flex'>
+      {/* side bar  */}
+
+      <aside className='w-64 bg-gray-900 text-white p-4 space-y-4 '>
+        <h2 className='text-3xl font-bold mb-6'>Admin Panel</h2>
+
+        <nav className='flex flex-col gap-2'>
+          <Link
+            className='hover:bg-gray-700 p-2 rounded'
+            href='/dashboard/admin'
+          >
+            Dashbord
+          </Link>
+
+          <Link
+            className='hover:bg-gray-700 px-3 py-2 rounded'
+            href='/dashboard/admin/users'
+          >
+            Users
+          </Link>
+          <Link
+            className='hover:bg-gray-700 px-3 py-2 rounded'
+            href='/dashboard/admin/courses'
+          >
+            Courses
+          </Link>
+          <Link
+            className='hover:bg-gray-700 px-3 py-2 rounded'
+            href='/dashboard/admin/students'
+          >
+            Students
+          </Link>
+          <Link
+            className='hover:bg-gray-700 px-3 py-2 rounded'
+            href='/dashboard/admin/teachers'
+          >
+            Teachers
+          </Link>
+        </nav>
+      </aside>
+      {children}
+    </div>
+  )
+}
